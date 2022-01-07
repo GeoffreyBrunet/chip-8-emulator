03bd4b0ac3544f3d68d5a3587a000b541b49ff13
Geoffrey Brunet <geoffrey.brunet@icloud.com>
2022-01-07 20:37:17 +0100

    finished map key to keyMap

--- src/Keyboard.ts
+++ src/Keyboard.ts
@@ -1,9 +1,14 @@
+import { keyMap } from "./constants/keyboardConstants";
+
 export class Keyboard {
     constructor() {
         document.addEventListener("keydown", this.keydown);
     }
 
-    keydown(event: KeyboardEvent) {
-        console.log('key', event.key);
+    keydown({key}: KeyboardEvent) {
+        const keyIndex = keyMap.findIndex((mapKey) => mapKey === key.toLowerCase());
+        if (keyIndex > -1) {
+            console.log('Find key', key, ' map to ', keyIndex);
+        } 
     }
 }
