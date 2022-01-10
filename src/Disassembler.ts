866f29471bc89f08c5d1e1bcc5a49c86eef8e0fd
Geoffrey Brunet <geoffrey.brunet@icloud.com>
2022-01-10 21:57:17 +0100

    instructions added, disassembler update

--- src/Disassembler.ts
+++ src/Disassembler.ts
@@ -1,10 +1,13 @@
 import { INSTRUCTION_SET } from "./constants/instructionSet";
 
 export class Disassembler {
-    disassemble(opcode: number) {
-        const instruction = INSTRUCTION_SET.find(
-            instruction => (opcode & instruction.mask) === instruction.pattern
-        );
-        console.log('instruction', instruction);
-    }
+   disassemble(opcode: number) {
+    const instruction = INSTRUCTION_SET.find(
+      (instruction) => (opcode & instruction.mask) === instruction.pattern
+    );
+    const args = instruction?.arguments.map(
+      (arg) => (opcode & arg.mask) >> arg.shift
+    );
+    return { instruction, args };
+  }
 }
