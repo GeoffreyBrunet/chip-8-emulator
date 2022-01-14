90f48b7b770a841105727cbf75619f2e7d89c2f5
Geoffrey Brunet <geoffrey.brunet@icloud.com>
2022-01-14 17:58:41 +0100

    write runChip8 function

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,21 @@ async function runChip8() {
   const arrayBuffer = await rom.arrayBuffer();
   const romBuffer = new Uint8Array(arrayBuffer);
   const chip8 = new Chip8(romBuffer);
-  chip8.registers.PC = 0x006;
-  chip8.registers.V[5] = 0xfe;
-  chip8.registers.V[8] = 0x12;
-  chip8.execute(0x8854);
-  console.log("V5", chip8.registers.V[5].toString(16));
-  console.log("V8", chip8.registers.V[8].toString(16));
-  console.log("VF", chip8.registers.V[0x0f].toString(16));
+  while (1) {
+    await chip8.sleep(200);
+    if (chip8.registers.DT > 0) {
+      await chip8.sleep();
+      chip8.registers.DT--;
+    }
+    if (chip8.registers.ST > 0) {
+      chip8.soundCard.enableSound();
+      await chip8.sleep();
+      chip8.registers.ST--;
+    }
+    if (chip8.registers.ST === 0) {
+      chip8.soundCard.disableSound();
+    }
+  }
 }
 
 runChip8();
